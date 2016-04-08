7ef564201d0111b8630b006d550185b72b7030cf
bellking <bellking@sk.com>
2016-04-08 17:48:28 +0900

    Pandas Study

---
+++ study/DataStructure.py
@@ -0,0 +1,53 @@
+from pandas import Series, DataFrame
+import matplotlib.pyplot as plt
+
+# kakao = Series([92600, 92400, 92100, 94300, 92300])
+# print(kakao)
+# 
+# kakao2 = Series([92600, 92400, 92100, 94300, 92300], index=['2016-02-19',
+#                                                             '2016-02-18',
+#                                                             '2016-02-17',
+#                                                             '2016-02-16',
+#                                                             '2016-02-15'])
+# print(kakao2)
+# 
+# mine = Series([10, 20, 30], index=['naver', 'sk', 'kt'])
+# wife = Series([10, 30, 20], index=['kt', 'naver', 'sk'])
+# family = mine + wife
+# print(family)
+# 
+# 
+# raw_data = {'col0': [1, 2, 3, 4],
+#             'col1': [10, 20, 30, 40],
+#             'col2': [100, 200, 300, 400]}
+# 
+# data = DataFrame(raw_data)
+# print(data)
+# print(data['col0'])
+# print(type(data['col0']))
+# print(type(data))
+
+daeshin = {'open':  [11650, 11100, 11200, 11100, 11000],
+           'high':  [12100, 11800, 11200, 11100, 11150],
+           'low' :  [11600, 11050, 10900, 10950, 10900],
+           'close': [11900, 11600, 11000, 11100, 11050]}
+date = ['16.02.29', '16.02.26', '16.02.25', '16.02.24', '16.02.23']
+daeshin_day = DataFrame(daeshin, columns=['open', 'high', 'low', 'close'], index=date)
+print(daeshin_day)
+print(daeshin_day['close'])
+print(list(daeshin_day['close']))
+
+# close = daeshin_day['close']
+# print(close)
+# 
+# day_data = daeshin_day.ix['16.02.24']
+# print(day_data)
+# print(type(day_data))
+# 
+# print(daeshin_day.columns)
+# print(daeshin_day.index)
+
+# plt.plot(list(daeshin_day['close']))
+# plt.show()
+
+
